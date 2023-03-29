5a90ac0f9d07275bb5886e01d28d653c1976828c
Pablo Araujo Camacho <pabloj2001@gmail.com>
2023-03-29 17:49:51 -0400

    hopefully fixed solidity issues

--- Etherium_Contract/hello-world/hardhat.config.js
+++ Etherium_Contract/hello-world/hardhat.config.js
@@ -8,13 +8,14 @@ require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-   solidity: "0.7.3",
+   solidity: "0.8.8",
    defaultNetwork: "goerli",
    networks: {
       hardhat: {},
       goerli: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         accounts: [`0x${PRIVATE_KEY}`],
+		 chainId: 5
       }
    },
 }
