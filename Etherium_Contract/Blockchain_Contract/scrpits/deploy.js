07e61d54fe456c81f226c02898415439e859be79
dhruvel <saga2500@mylaurier.ca>
2023-03-29 19:07:53 -0400

    made the BlockChain-DNS contract

---
+++ Etherium_Contract/Blockchain_Contract/scrpits/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  // We get the contract to deploy
+  const Blockchain_DNS = await ethers.getContractFactory("BlockChain_DNS");
+  
+  // Start deployment, returning a promise that resolves to a contract object
+  const blockchain_dns = await Blockchain_DNS.deploy("Hello, World!");
+  console.log("Blockchain_DNS deployed to:", blockchain_dns.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
