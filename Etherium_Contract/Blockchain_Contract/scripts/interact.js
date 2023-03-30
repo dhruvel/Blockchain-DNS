b8838abe40664b39e015d579480f96b222f2126d
dhruvel <saga2500@mylaurier.ca>
2023-03-29 20:32:48 -0400

    fixing interact.js .... and failing

--- Etherium_Contract/Blockchain_Contract/scripts/interact.js
+++ Etherium_Contract/Blockchain_Contract/scripts/interact.js
@@ -5,7 +5,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 // For Hardhat
-const contract = require("../artifacts/contracts/BlockChain_DNS.sol/BlockChain_DNS.json");
+const contract = require("../artifacts/contracts/blockchain_contract.sol/BlockChain_DNS.json");
 
 // provider - Alchemy
 const alchemyProvider = new ethers.providers.AlchemyProvider(network="goerli", API_KEY);
@@ -18,14 +18,14 @@ const blockchain_dns = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signe
 
 // interact with blockchain_dns
 async function main() {
-    const message = await helloWorldContract.message();
+    const message = await blockchaindnscontract.message();
     console.log("The message is:", + message);
 
     console.log("Updating the message...");
-    const tx = await helloWorldContract.update("This is the new message!");
+    const tx = await blockchaindnscontract.update("This is the new message!");
     await tx.wait();
 
-    const newMessage = await helloWorldContract.message();
+    const newMessage = await blockchaindnscontract.message();
     console.log("The new message is:", + newMessage);
 }
 
