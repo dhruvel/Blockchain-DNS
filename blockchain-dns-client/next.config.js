beaad1b995d24ca71aa159c7be6738fc996be8a4
Pablo Araujo Camacho <pabloj2001@gmail.com>
2023-04-03 15:57:47 -0400

    No cache

--- blockchain-dns-client/next.config.js
+++ blockchain-dns-client/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: () => [
+    {
+      source: '/api/domains',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
 }
 
 module.exports = nextConfig
